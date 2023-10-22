ff94e7d801bc44297c4d784362171d11392a59f6
venkatakrishnadvk <63203420+venkatakrishnadvk@users.noreply.github.com>
2023-10-22 12:20:04 +0000

    change 2

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
    <head>
       <title>ToDoList</title>
+      <title>ToDoListByDVK</title>
       <link rel="stylesheet" type="text/css" href="style.css">
       <meta name='viewport' content='width=device-width, initial-scale=1'>
       <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
